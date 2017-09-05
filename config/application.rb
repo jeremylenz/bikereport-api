d1a1ef62f2591c60553f2f67e7745d74f715215d
jeremylenz <jeremylenz@gmail.com>
2017-09-05 13:13:53 -0400

    troubleshooting cors

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module BikereportApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :delete, :patch]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :options]
       end
     end
 
