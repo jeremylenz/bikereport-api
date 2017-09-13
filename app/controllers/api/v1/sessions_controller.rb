76d8fb6f1c0c8ca3b1351e3f553f6bdcda57d632
jeremylenz <jeremylenz@gmail.com>
2017-09-13 18:25:02 -0400

    i hate require_relative

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,23 @@
-require_relative '/../../../auth.rb'
+require 'jwt'
+
+class Auth
+
+  ALGORITHM = 'HS256'
+
+  def self.issue(payload)
+    JWT.encode(payload, auth_secret, ALGORITHM)
+  end
+
+  def self.decode(token)
+    JWT.decode(token, auth_secret, true, {algorithm: ALGORITHM}).first
+  end
+
+  def self.auth_secret
+    ENV["AUTH_SECRET"]
+  end
+
+end # of class
+
 
 class Api::V1::SessionsController < ApplicationController
 
