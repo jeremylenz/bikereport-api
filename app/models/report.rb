58c1e0dd86e826e343d841eefd197b78a6f3a8f9
Jeremy Lenz <jeremylenz@gmail.com>
2018-04-10 13:45:43 -0400

    Increment likes in a safer way

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,6 +5,16 @@ class Report < ApplicationRecord
   belongs_to :location
   has_one :image
 
+  before_update :check_likes
+
   accepts_nested_attributes_for :bike_path, :user, :location, :image
 
+  private
+
+    def check_likes
+      # Increment likes by 1, no matter what the front-end thinks the new number of likes is.
+      if self.changes.include?('likes')
+        self.likes = self.changed_attributes[:likes] + 1 #changed_attributes is the original value before change
+      end
+    end
 end
